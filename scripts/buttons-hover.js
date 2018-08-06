fb8882b0b53872355a2c90ca05268c43daa6cc4c
Dawidd95 <32812931+Dawidd95@users.noreply.github.com>
2018-08-06 19:55:46 +0200

    Add files via upload

---
+++ scripts/buttons-hover.js
@@ -0,0 +1,30 @@
+function init() {
+	let portfolioBtn = document.querySelector(".buttons__portfolio");
+	let contactBtn = document.querySelector(".buttons__contact");
+	let buttonsSection = document.querySelector(".page-head__buttons");
+
+	contactBtn.addEventListener("mouseover", function() {
+		portfolioBtn.style.backgroundColor = "transparent";
+		portfolioBtn.style.color = "white";
+		contactBtn.style.backgroundColor = "#9bd3d9";
+		contactBtn.style.color = "black";
+	});
+
+	portfolioBtn.addEventListener("mouseover", function() {
+		portfolioBtn.style.backgroundColor = "#9bd3d9";
+		portfolioBtn.style.color = "black";
+		contactBtn.style.backgroundColor = "transparent";
+		contactBtn.style.color = "white";
+	});
+
+	buttonsSection.addEventListener("mouseout", function() {
+		portfolioBtn.style.backgroundColor = "transparent";
+		portfolioBtn.style.color = "white";
+		portfolioBtn.style.transition = "0.5s";
+		contactBtn.style.backgroundColor = "#9bd3d9";
+		contactBtn.style.color = "white";
+		contactBtn.style.transition = "0.5s";
+	});
+}
+
+document.addEventListener("DOMContentLoaded", init);
